1c885f4afaad01865fa7aa847e2e078cf6acc11e
woon <gilwoon.work@gmail.com>
2023-01-05 17:14:29 +0900

    provisioning reset push notification service module reset

---
+++ org.bgent.otherhome.ios.NotificationService/NotificationService.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationService.swift
+//  org.bgent.otherhome.ios.NotificationService
+//
+//  Created by Gilwoon on 2023/01/03.
+//  Copyright © 2023 Begent Group. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        if let bestAttemptContent = bestAttemptContent {
+            // Modify the notification content here...
+            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
+            
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
